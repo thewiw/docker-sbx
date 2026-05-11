f1bbfdf25400b0fdfeed4d18d64d2f158777651b
RW WiW <regis.wernet@thewiw.com>
2026-05-11 16:04:01 +0200

    Added secrets detection when creating the sandbox

--- docker-sbx-create-sandbox.sh
+++ docker-sbx-create-sandbox.sh
@@ -8,14 +8,16 @@ set -Eeuo pipefail   # Safer Bash: fail on errors, unset vars, pipeline failures
 name=""
 path=""
 envfile=""
+secrets="true"
 
 usage() {
   scriptname=`basename "$0"`
-  echo "Usage: $scriptname -n [sandbox name] -p [project path] -e [sandbox env file]"
+  echo "Usage: $scriptname -n [sandbox name] -p [project path] -e [sandbox env file] -s [check secrets]"
   echo "Options:"
-  echo "  -n string  Define the name of the new sandbox [mandatory]"
-  echo "  -p string  Absolute path of project's directory [mandatory]"
-  echo "  -e string  Env file [optional]"
+  echo "  -n string     Define the name of the new sandbox [mandatory]"
+  echo "  -p string     Absolute path of project's directory [mandatory]"
+  echo "  -e string     Env file [optional]"
+  echo "  -s true/false Check for secrets (default true)"
   echo ""
   echo "  -h, --help Show this help and exit"
   echo ""
@@ -26,19 +28,80 @@ usage() {
   exit 1
 }
 
+check_project_secrets_files() {
+  echo "Looking for secrets files in $path, please wait..."
+
+  secrets=`find "$path" -type f \( -name "google-services.json" -o -name "key.properties" \)`
+  if echo "$secrets" 2> /dev/null | grep -q .; then
+    echo "$secrets"
+    echo "Error: secrets file(s) detected within this project, make sure those can not be reached from sandbox and run this command again"
+    echo ""
+    exit 1
+  else
+    echo "No secrets file detected within this project, let's continue"
+    echo ""
+  fi
+}
+
+check_project_secrets_git() {
+  gitrepo=""
+  if [ -d "$path/.git" ]; then
+    gitrepo="$path/.git"
+  elif gitdir=$(find "$path" -type d -name .git -print -quit 2>/dev/null); then
+    gitrepo="$gitdir"
+  fi
+
+  if [ -n "$gitrepo" ]; then
+    echo "GIT repository found at ${gitrepo}, running leaks detection, please wait..."
+    if ! docker run --rm -v "$gitrepo":/repo -v "$(pwd)":/report ghcr.io/gitleaks/gitleaks:latest detect --source /repo --report-path /report/report.json; then
+      echo "Error: detected at least 1 leak in git repository ${gitrepo}, fix it/them before running sandbox"
+      echo ""
+      exit 1
+    else
+      echo ""
+    fi
+  else
+    echo "GIT repository not found, not running leaks detection"
+    echo ""
+  fi
+}
+
+check_project_secrets() {
+  if [[ "${secrets,,}" != "false" ]]; then
+    echo ""
+    echo "════════════════════════"
+    echo "Checking project secrets $name"
+    echo "════════════════════════"
+    echo ""
+
+    check_project_secrets_files
+    check_project_secrets_git
+  else
+    echo ""
+    echo "═════════════════════════════════════════════════════════════════════════════════════════"
+    echo "Not checking project secrets, MAKE SURE NO SECRET CAN BE LEAKED THROUGHT THIS PROJECT !!!"
+    echo "═════════════════════════════════════════════════════════════════════════════════════════"
+    echo ""
+  fi
+}
+
 create_sandbox() {
   prjname=`basename "$path"`
 
-  echo "════════════════════"
-  echo "⚙️  rreating sandbox $name"
-  echo "════════════════════"
+  echo ""
+  echo "════════════════"
+  echo "Creating sandbox $name"
+  echo "════════════════"
+  echo ""
   sbx create --name "$name" claude "$path"
 }
 
 setup_sandbox() {
-  echo "════════════════════"
-  echo "⚙️  rpdating sandbox $name"
-  echo "════════════════════"
+  echo ""
+  echo "════════════════"
+  echo "Updating sandbox $name"
+  echo "════════════════"
+  echo ""
 
   sbx cp ./docker-sbx-setup-sandbox.sh "$name":../setup-sandbox.sh
   sbx exec -ti "$name" chmod 755 ../setup-sandbox.sh
@@ -50,22 +113,25 @@ setup_sandbox() {
   fi
 }
 
-while getopts ":n:p:e:h" opt; do
+while getopts ":n:p:e:s:h" opt; do
     case "$opt" in
         n) name="$OPTARG";;
         p) path="$OPTARG";;
         e) envfile="$OPTARG";;
+        s) secrets="$OPTARG";;
         h) usage;;
     esac
 done
 
 if [[ -z "$name" || -z "$path" ]]; then
-  echo "══════════════════════════════════════════════════════════════════════════════════"
-  echo "⚠️  Missing parameter, you must at least provide a sandbox name and a project path"
-  echo "══════════════════════════════════════════════════════════════════════════════════"
+  echo ""
+  echo "══════════════════════════════════════════════════════════════════════════════"
+  echo "Missing parameter, you must at least provide a sandbox name and a project path"
+  echo "══════════════════════════════════════════════════════════════════════════════"
   echo ""
   usage
 fi
 
+check_project_secrets
 create_sandbox
 setup_sandbox
